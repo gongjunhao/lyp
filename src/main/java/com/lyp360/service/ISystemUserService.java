8435ad79678a464979d1148cf43819908961df36
gongjunhao <gongjunhao@sina.com>
2016-04-25 23:12:52 +0800

    init

--- src/main/java/com/lyp360/service/ISystemUserService.java
+++ src/main/java/com/lyp360/service/ISystemUserService.java
@@ -1,5 +1,7 @@
 package com.lyp360.service;
 
+import com.lyp360.entity.Systemuser;
+
 /**
  * Created by GongJunhao on 2016/4/25.
  * ISystemUserService
--- src/test/java/com/lyp360/test/SystemUserServiceTest.java
+++ src/test/java/com/lyp360/test/SystemUserServiceTest.java
@@ -14,10 +14,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class SystemUserServiceTest {
+
     @Autowired
-    //private SystemuserMapper userDao;
+    private SystemuserMapper userDao;
 
     @Test
     public void insertUser(){
+        Systemuser systemuser = new Systemuser();
+        systemuser.setId(11l);
+        systemuser.setLoginname("admin");
+        userDao.insert(systemuser);
     }
 }
